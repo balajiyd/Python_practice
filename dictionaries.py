1a84b6eb1757f949980988494fe9aad8da041461
balajiyd <124142672+balajiyd@users.noreply.github.com>
2023-11-30 18:35:30 +0530

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,16 @@
+monthcon = {
+    "Jan" : 'January',
+    'Feb' : "February",
+    "Mar" : 'March',
+    "Apr" : "April",
+    "May" : "May",
+    "Jun" : "June",
+    "Jul" : "July",
+    "Aug" : "August",
+    "Sep" : "September",
+    "oct" : "October",
+    "Nov" : "November",
+    "Dec" : "December",
+}
+
+print(monthcon.get("Dec"))
