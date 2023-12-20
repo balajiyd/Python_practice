3c75482694f5692ada9394c2853e71da1f58bee5
balajiyd <124142672+balajiyd@users.noreply.github.com>
2023-12-20 14:34:39 +0530

    Create translator.py

---
+++ translator.py
@@ -0,0 +1,10 @@
+def translate(phrase):
+    translation = ''
+    for letter in phrase:
+        if letter in 'aeiouAEIOU':
+            translation = translation +'g'
+        else:
+            translation = translation +letter
+    return translation
+
+print(translate(input("ENter a phrase: ")))
